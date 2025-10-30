ca5d61dd9d1385947d38ada7aaf26ec980e5ebd3
Enrico Marques <enricothulio.marques@gmail.com>
2025-10-30 09:48:25 -0300

    fix fim

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,9 @@
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const prisma = new PrismaClient().$extends(withAccelerate())
+const prisma = new PrismaClient({
+     datasourceUrl: process.env.DATABASE_URL, // prisma+postgr
+}).$extends(withAccelerate())
 
 
 export { prisma };
