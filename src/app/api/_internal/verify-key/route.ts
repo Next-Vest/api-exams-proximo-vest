7c1e55a4573ee8fc2579b7a25d1227860db1c762
Enrico Marques <enricothulio.marques@gmail.com>
2025-10-30 00:59:09 -0300

    fix

--- src/app/api/_internal/verify-key/route.ts
+++ src/app/api/_internal/verify-key/route.ts
@@ -1,6 +1,6 @@
 // app/api/_internal/verify-key/route.ts
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "../../../../lib/prisma";
+import {prisma} from "../../../../lib/prisma";
 
 export const runtime = "nodejs";
 
